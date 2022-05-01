ed9297157c9db40faaf6d0076c656fd50749e16f
DevMohi <morphex72@gmail.com>
2022-05-01 06:55:31 +0600

    login and register implemented also verification added

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,28 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../../Firebase/Firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Login = () => {
+    const navigate = useNavigate()
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        firebaseError,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const [userInfo, setUserInfo] = useState({
+        email: "",
+        password: "",
+        confirmPassword: "",
+    })
+
+    const [errors, setErrors] = useState({
+        email: "",
+        password: "",
+        general: "",
+    });
+
+    const handleEmail = (e) => {
+        const emailRegex = /\S+@\S+\.\S+/;
+        const valid = emailRegex.test(e.target.value);
+        console.log(e.target.value)
+
+        if (valid) {
+            setUserInfo({ ...userInfo, email: e.target.value });
+            setErrors({ ...errors, email: "" })
+        }
+        else if (e.target.value === '') {
+            setErrors({ ...errors, email: "" })
+        }
+
+        else {
+            setErrors({ ...errors, email: "Invalid email" })
+        }
+    }
+
+    const handlePassword = (e) => {
+        const passRegex = /.{8,}/;
+        const validPass = passRegex.test(e.target.value);
+
+        if (validPass) {
+            setUserInfo({ ...userInfo, password: e.target.value })
+            setErrors({ ...errors, password: "" })
+        }
+        else if (e.target.value === '') {
+            setErrors({ ...errors, password: "" })
+        }
+
+
+        else {
+            setErrors({ ...errors, password: "less than 8 characters" })
+        }
+    }
+    const handleLogin = e => {
+        e.preventDefault()
+        signInWithEmailAndPassword(userInfo.email, userInfo.password)
+    }
+
+    if (user) {
+        navigate('/')
+    }
     return (
         <div className='container mt-5'>
             <div className='w-50 mx-auto'>
                 <h1>Login</h1>
-                <Form>
+                <Form onSubmit={handleLogin}>
                     <Form.Group className="mb-3" controlId="formGroupEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control type="email" placeholder="Enter email" required onChange={handleEmail} />
+                        {
+                            errors?.email && <p className='text-danger'>{errors.email}</p>
+                        }
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" required
+                            onChange={handlePassword} />
+                        {
+                            errors?.password && <p className='text-danger'>
+                                {errors.password}
+                            </p>
+                        }
                     </Form.Group>
                     <button className='btn btn-dark mx-auto w-50 d-block'>Login</button> <br />
                     <div className='text-center mb-1'>
                         <span >New to Stonks? <Link to='/register'>Register</Link></span> <br />
-                        <span>Forgot Password? <a href="">Reset</a></span>
+                        <span>Forgot Password? <a >Reset</a></span>
                     </div>
                 </Form>
+                {
+                    firebaseError && <p className='text-danger text-center'>{firebaseError.message}</p>
+                }
 
                 <SocialLogin></SocialLogin>
             </div>
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,135 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form } from 'react-bootstrap';
+import { useCreateUserWithEmailAndPassword, } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../../Firebase/Firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
+
 
 const Register = () => {
+    const navigate = useNavigate()
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        firebaseError,
+    ] = useCreateUserWithEmailAndPassword(auth, {
+        sendEmailVerification: true
+    });
+
+    const [userInfo, setUserInfo] = useState({
+        email: "",
+        password: "",
+        confirmPassword: "",
+    })
+
+    const [errors, setErrors] = useState({
+        email: "",
+        password: "",
+        general: "",
+    });
+
+    const handleEmail = (e) => {
+        const emailRegex = /\S+@\S+\.\S+/;
+        const valid = emailRegex.test(e.target.value);
+        console.log(e.target.value)
+
+        if (valid) {
+            setUserInfo({ ...userInfo, email: e.target.value });
+            setErrors({ ...errors, email: "" })
+        }
+        else if (e.target.value === '') {
+            setErrors({ ...errors, email: "" })
+        }
+
+        else {
+            setErrors({ ...errors, email: "Invalid email" })
+        }
+    }
+
+    const handlePassword = (e) => {
+        const passRegex = /.{8,}/;
+        const validPass = passRegex.test(e.target.value);
+
+        if (validPass) {
+            setUserInfo({ ...userInfo, password: e.target.value })
+            setErrors({ ...errors, password: "" })
+        }
+        else if (e.target.value === '') {
+            setErrors({ ...errors, password: "" })
+        }
+
+
+        else {
+            setErrors({ ...errors, password: "less than 8 characters" })
+        }
+    }
+
+    const handleConfirmPass = (e) => {
+        if (userInfo.password === e.target.value) {
+            setUserInfo({ ...userInfo, confirmPassword: e.target.value })
+
+            setErrors({ ...errors, password: "" })
+        }
+        else {
+            setErrors({ ...errors, password: "Passwords Dont match" })
+        }
+    }
+
+
+    const handleRegister = (e) => {
+        e.preventDefault()
+        createUserWithEmailAndPassword(userInfo.email, userInfo.password)
+
+    }
+    if (user) {
+        navigate('/')
+    }
+
     return (
-        <div>
-            <h1>Register</h1>
+        <div className='container mt-5'>
+            <div className='w-50 mx-auto'>
+                <h1>Register</h1>
+                <Form onSubmit={handleRegister}>
+                    <Form.Group className="mb-3" controlId="formGroupEmail">
+                        <Form.Label >Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email"
+                            required
+                            onChange={handleEmail}
+                        />
+                        {
+                            errors?.email && <p className='text-danger'>{errors.email}</p>
+                        }
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroupPassword">
+                        <Form.Label >Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password"
+                            required
+                            onChange={handlePassword} />
+                        {
+                            errors?.password && <p className='text-danger'>
+                                {errors.password}
+                            </p>
+                        }
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroupPassword2">
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password"
+                            required
+                            onChange={handleConfirmPass}
+                        />
+                    </Form.Group>
+                    <button className='btn btn-dark mx-auto w-50 d-block'>Register</button> <br />
+                    <div className='text-center mb-1'>
+                        <span >Already have an account? <Link to='/login'>Login</Link></span> <br />
+                    </div>
+                </Form>
+                {
+                    firebaseError && <p className='text-danger text-center'>{firebaseError.message}</p>
+                }
+
+                <SocialLogin></SocialLogin>
+            </div>
         </div>
     );
 };
