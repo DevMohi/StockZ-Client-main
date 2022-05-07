078d6a77ae96ff1db698be5244f6954cb3b7c749
DevMohi <morphex72@gmail.com>
2022-05-07 18:31:20 +0600

    restock added

--- src/Pages/UpdateDetails/UpdateDetails.js
+++ src/Pages/UpdateDetails/UpdateDetails.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Button, Form, Modal } from 'react-bootstrap';
+import { Link, useParams } from 'react-router-dom';
 
 const UpdateDetails = () => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     const { inventoryId } = useParams()
     const [inventory, setInventory] = useState({})
     const [sold, setSold] = useState("")
@@ -35,8 +40,25 @@ const UpdateDetails = () => {
 
     }
 
-
-
+    const handleStock = e => {
+        e.preventDefault()
+        const stock = parseInt(e.target.stock.value)
+        console.log(stock)
+        const increaseQuantity = parseInt(inventory.quantity) + stock;
+        const updateQuantity = { quantity: increaseQuantity.toString() }
+        const url = `http://localhost:5000/inventory/${inventoryId}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updateQuantity)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }
 
     return (
         <div className="container mt-5">
@@ -53,9 +75,39 @@ const UpdateDetails = () => {
                         <h5 className='pb-2'>Supplier: {inventory.supplier}</h5>
                         <h5 className='pb-2'>Price: {inventory.price}</h5>
                         <button className='btn btn-success me-2' onClick={updateBtn}>Delivered</button>
-                        <button className='btn btn-danger'>Restock</button>
+                        <Button variant="danger" onClick={handleShow}>
+                            Restock
+                        </Button>
                         {sold && <p className='text-danger'>{sold}</p>}
 
+
+                        <Modal show={show} onHide={handleClose}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Restock</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <div className='w-50 mx-auto'>
+                                    <Form onSubmit={handleStock}>
+                                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                                            <Form.Control type="number" placeholder="Add the quantity"
+                                                name='stock'
+                                            />
+                                            <div className='mt-2'>
+                                                <Button className='mx-auto d-block btn btn-danger' variant="primary" type="submit">
+                                                    Restock
+                                                </Button>
+                                            </div>
+
+                                        </Form.Group>
+                                    </Form>
+                                </div>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="secondary" onClick={handleClose}>
+                                    Close
+                                </Button>
+                            </Modal.Footer>
+                        </Modal>
 
                     </div>
 
